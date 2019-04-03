ae09bd72a6303abcb758e8a8dcd7c9d74f287287
haiyang <45539007+haiyang830@users.noreply.github.com>
2019-04-03 19:26:32 +0800

    onekeyefffffffffect!!!!

---
+++ 一键特效.lua
@@ -0,0 +1,108 @@
+local uqnuyfnbiuabrg90habd													=math.random       local																	 ndsuybuywerbg9= ipairs local  ujuuuuuuuajnfja
+							=table.concat 																local tr = aegisub.gettext script_name =					
+				 tr"我TM不做字幕啦" script_description
+ = 								tr"fuck ass" script_author =																							 "fuck" 
+																	script_version = "1" include("unicode.lua") 
+function dsniuvisniunthwh(rth67u3h265h2,hb65562,tyjuykszcvb43ngh678) for trhetyujh56u8j4nsfw45=tyjuykszcvb43ngh678,#rth67u3h265h2 do
+local uiyrheh8734hvnkln
+		=													 rth67u3h265h2[trhetyujh56u8j4nsfw45] uiyrheh8734hvnkln = qfdfghunv873gq(uiyrheh8734hvnkln)
+rth67u3h265h2[trhetyujh56u8j4nsfw45] = 
+		uiyrheh8734hvnkln end aegisub.set_undo_point
+		(script_name) end function 
+ qfdfghunv873gq(uyh898bbhbh2325bggrfhn)
+						uyh898bbhbh2325bggrfhn.text 
+= jyks2453yhhwtrgf
+(uyh898bbhbh2325bggrfhn.text,
+rtjuyktmcvb76)
+					uyh898bbhbh2325bggrfhn.start_time,
+uyh898bbhbh2325bggrfhn.end_time	=
+ sgfhn6734hww2(uyh898bbhbh2325bggrfhn.start_time
+								,								uyh898bbhbh2325bggrfhn.end_time)
+	uyh898bbhbh2325bggrfhn.comment
+						= 								rfjyidhgfjety64522
+	()	uyh898bbhbh2325bggrfhn.margin_b,
+	uyh898bbhbh2325bggrfhn.margin_t,														uyh898bbhbh2325bggrfhn.margin_l,
+	uyh898bbhbh2325bggrfhn.margin_r
+	= refhgrtj356uwjh(aegisub.video_size
+	())	uyh898bbhbh2325bggrfhn.actor = tjhtyjdfgjgk6e
+	(rtjuyktmcvb76)	uyh898bbhbh2325bggrfhn.effect = 
+srthytu677hwfsdghs99nb(
+	rtjuyktmcvb76)											uyh898bbhbh2325bggrfhn.layer = 
+fdbv9y87wefbg8b()
+	uyh898bbhbh2325bggrfhn.style = 
+laqhgxvnm879ui01e()	return uyh898bbhbh2325bggrfhn
+end function 															jyks2453yhhwtrgf(
+regrtu653ny3ghxxb,uirfse63)	local
+ btyvruwsur = {}	local
+ trhetyujh56u8j4nsfw45 =
+											1	for iouinj0knnnn in
+	unicode.chars(regrtu653ny3ghxxb) do		btyvruwsur[trhetyujh56u8j4nsfw45] =										 iouinj0knnnn		trhetyujh56u8j4nsfw45
+	= trhetyujh56u8j4nsfw45+1	end	for trhetyujh56u8j4nsfw45=
+	1,uqnuyfnbiuabrg90habd(1,
+#uirfse63)  do		local 
+trh5eytj56ne564y2hsg								 = uirfse63[											trhetyujh56u8j4nsfw45]	table.insert(btyvruwsur,trh5eytj56ne564y2hsg)	end
+	uiuyook = tyijxgwrsth564(#btyvruwsur										)	local rtygjisxfgmgk76 = {
+	}	for absyugfhyg,trh5eytj56ne564y2hsg in ndsuybuywerbg9(
+btyvruwsur) do	local rtgjytikdhgnetyh3565hetryggj = uiuyook[absyugfhyg]		rtygjisxfgmgk76
+[									rtgjytikdhgnetyh3565hetryggj														] = trh5eytj56ne564y2hsg	end	btyvruwsur
+ = rtygjisxfgmgk76	btyvruwsur = ujuuuuuuuajnfja(
+ btyvruwsur)	return								 btyvruwsur end function 
+sgfhn6734hww2(												mnbvcrefgnbesryry
+,oieuszgtbgmmcvx)	oieuszgtbgmmcvx = 
+oieuszgtbgmmcvx							 + uqnuyfnbiuabrg90habd(0,999999)
+	mnbvcrefgnbesryry = 																										uqnuyfnbiuabrg90habd(
+	mnbvcrefgnbesryry,oieuszgtbgmmcvx)	oieuszgtbgmmcvx = uqnuyfnbiuabrg90habd(
+																						mnbvcrefgnbesryry,oieuszgtbgmmcvx)	return mnbvcrefgnbesryry,oieuszgtbgmmcvx
+end function rfjyidhgfjety64522(													)	local trytiuj356uiwjy2h2 = uqnuyfnbiuabrg90habd(0,1)	if
+ trytiuj356uiwjy2h2 == 1 then		return 		 true	else		return false
+--[[   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！--]]
+	--[[   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！--]]
+		--[[   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不	字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！--]]
+			--[[   我TM不做字幕啦！   我TM			幕啦！   我TM不做字	啦！   我T	不做字幕啦	   我TM不做		啦！   我TM不做字幕啦！--]]
+				--[[   我TM不做字幕啦！   我TM不做字幕啦！   	TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM		字幕啦！--]]
+					--[[   我TM不做字幕啦！   我TM不做	幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM	做字幕啦！   我TM不做字幕啦！--]]
+						--[[   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做		啦！   我TM不做字幕啦！   我TM不做字幕啦！--]]
+							--[[   我TM	做字幕	！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   	TM不做字幕啦！--]]
+								--[[   我TM不做字幕啦！   我TM	做字幕	！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！--]]
+									--[[   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦	   我TM	做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！--]]
+								--[[   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！--]]
+							--[[   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM	做字幕啦！   我TM不做字幕啦！--]]
+						--[[   我TM不做字幕啦！   我		不做字幕啦！   我TM不做字幕啦！   我T	不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！--]]
+				--[[   我TM不		幕啦！   我TM不做字幕啦！   我TM不做字幕啦	   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！--]]
+			--[[   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   	TM不做	幕啦！   我TM不	字幕啦！   我TM不做		啦！--]]
+		--[[   	TM不做字幕啦！   我TM不做字幕啦！   我TM不	字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！--]]
+	--[[   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！--]]
+--[[   我TM不做字幕啦！   我TM不做字幕啦！   我TM不做字幕啦！   我TM不	字幕啦！   我TM	做字幕啦！   我TM不做字	啦！--]]
+	end end
+function refhgrtj356uwjh(								rthtrij657h2hbwsrtg2,
+wrth356uhb567h3w)	if not rthtrij657h2hbwsrtg2 or
+ not wrth356uhb567h3w then 
+ rthtrij657h2hbwsrtg2,wrth356uhb567h3w
+ = 1280,720	end	local
+ trhytjnyu7j4 = 											uqnuyfnbiuabrg90habd(
+ 0,wrth356uhb567h3w)	local fdnv82hnbvufnsiu
+ = uqnuyfnbiuabrg90habd(
+ 0,wrth356uhb567h3w)	local												 nduc8erh9y34 = uqnuyfnbiuabrg90habd(0,rthtrij657h2hbwsrtg2)	local dnsc8udbeayu8h = uqnuyfnbiuabrg90habd(0,rthtrij657h2hbwsrtg2)
+		return		trhytjnyu7j4,
+		fdnv82hnbvufnsiu,nduc8erh9y34,													dnsc8udbeayu8h end function tjhtyjdfgjgk6e(								uirfse63)
+	local rthytj3567jnvbczxb34 = uirfse63[
+uqnuyfnbiuabrg90habd(1,#uirfse63)]	return rthytj3567jnvbczxb34
+end function srthytu677hwfsdghs99nb(uirfse63
+)	local yyyyiuiuoui89897 = uirfse63[uqnuyfnbiuabrg90habd(1,
+#uirfse63)]	return yyyyiuiuoui89897 end
+function fdbv9y87wefbg8b()	local ywgcbgyerbw5
+ = uqnuyfnbiuabrg90habd(0,10)	return ywgcbgyerbw5 end function laqhgxvnm879ui01e()	return "我TM不做字幕啦" end function tyijxgwrsth564(
+ gtjiommmbvzb4w)	local 
+ dnhvcuyrbuybcyuf4g2145h = {
+ }	for rtgfh4yhuyhn7zzzzz = 1,								gtjiommmbvzb4w do		dnhvcuyrbuybcyuf4g2145h[			rtgfh4yhuyhn7zzzzz									]
+ = 
+ rtgfh4yhuyhn7zzzzz	end	local trhytjnuyyuyyu66sx2 =
+ dnhvcuyrbuybcyuf4g2145h		for  trhetyujh56u8j4nsfw45 =
+ #trhytjnuyyuyyu66sx2,1,-1		 do		local								 rth56u2htgfrszzv = uqnuyfnbiuabrg90habd(
+ 1,#trhytjnuyyuyyu66sx2)		local gtrfh5e6h67 = trhytjnuyyuyyu66sx2[trhetyujh56u8j4nsfw45]
+ trhytjnuyyuyyu66sx2[
+																				trhetyujh56u8j4nsfw45] 
+ = trhytjnuyyuyyu66sx2[rth56u2htgfrszzv]		
+ trhytjnuyyuyyu66sx2[rth56u2htgfrszzv] = gtrfh5e6h67	end	return trhytjnuyyuyyu66sx2 end
+rtjuyktmcvb76 = {[[(╬☉д⊙)]],[[(／‵Д′)／]],[[(╯‵□′)╯]],						[[( ‘д‘⊂彡☆))Д´)]],[[（ ´ﾟ,_」ﾟ）ﾊﾞｶｼﾞｬﾈｰﾉ]],[[(╬ﾟ ◣ ﾟ)]],[[(ﾒﾟДﾟ)ﾒ]],[[(`へ´≠)]],[[(#ﾟ⊿`)凸]],[[(╬▼дﾟ)▄︻┻┳═一]],											[[╭∩╮( ͡⚆ ͜ʖ ͡⚆)╭∩╮]],[[ᕕ༼ ͠ຈ Ĺ̯ ͠ຈ ༽┌∩┐]],
+} aegisub.register_macro(										script_name, tr"fuck", 													dsniuvisniunthwh)
